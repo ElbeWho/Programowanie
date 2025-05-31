5041e6e65d1aaa7c1bb94f92f43820bb224b3e2d
Gabriela Niechwiadowicz <120680947+ElbeWho@users.noreply.github.com>
2025-05-31 16:48:14 +0200

    Update wstep.cpp

--- grupa podstawowa/przygotowanie/wstep.cpp
+++ grupa podstawowa/przygotowanie/wstep.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <iomanip>
 
+//wersja poprawiona bez "switch", co nie poprawiło się na zajęciach
+
 using namespace std;
 
 int main() {
@@ -24,7 +26,6 @@ int main() {
     getline(file, linia);
     stringstream ss(linia);
     string kolumna;
-    cout << kolumna<< endl;
     while (ss >> kolumna) {
         naglowki.push_back(kolumna);
     }
@@ -47,55 +48,44 @@ int main() {
         return 0;
     }
 
-    // Menu użytkownika
+    cout << "\nMENU:\n";
+    cout << "1. Wyświetl średni obwód\n";
+    cout << "2. Zapisz boki b dla trójkątów, gdzie a > 5 i c > 5\n";
+    cout << "0. Wyjście\n";
+    cout << "Wybierz opcję: ";
+
     int wybor;
-    do {
-        cout << "\nMENU:\n";
-        cout << "1. Wyświetl średni obwód\n";
-        cout << "2. Zapisz boki b dla trójkątów, gdzie a > 5 i c > 5\n";
-        cout << "0. Wyjście\n";
-        cout << "Wybierz opcję: ";
-        cin >> wybor;
+    cin >> wybor;
 
-        switch (wybor) {
-            case 1: {
-                double suma = 0;
-                for (size_t i = 0; i < a_vec.size(); ++i) {
-                    suma += a_vec[i] + b_vec[i] + c_vec[i];
-                }
-                double srednia = suma / a_vec.size();
-                cout << fixed << setprecision(2);
-                cout << "Średni obwód: " << srednia << endl;
-                break;
-            }
-
-            case 2: {
-                ofstream output("bok_b.txt");
-                if (!output.is_open()) {
-                    cerr << "Nie można otworzyć pliku wyjściowego.\n";
-                    break;
-                }
-
-                for (size_t i = 0; i < a_vec.size(); ++i) {
-                    if (a_vec[i] > 5 && c_vec[i] > 5) {
-                        output << fixed << setprecision(2) << b_vec[i] << endl;
-                    }
-                }
-
-                output.close();
-                cout << "Zapisano wartości b do pliku bok_b.txt\n";
-                break;
-            }
-
-            case 0:
-                cout << "Zakończono program.\n";
-                break;
-
-            default:
-                cout << "Niepoprawny wybór.\n";
+    if (wybor == 1) {
+        double suma = 0;
+        for (size_t i = 0; i < a_vec.size(); ++i) {
+            suma += a_vec[i] + b_vec[i] + c_vec[i];
         }
-
-    } while (wybor != 0);
+        double srednia = suma / a_vec.size();
+        cout << fixed << setprecision(2);
+        cout << "Średni obwód: " << srednia << endl;
+    }
+    else if (wybor == 2) {
+        ofstream output("bok_b.txt");
+        if (!output.is_open()) {
+            cerr << "Nie można otworzyć pliku wyjściowego.\n";
+        } else {
+            for (size_t i = 0; i < a_vec.size(); ++i) {
+                if (a_vec[i] > 5 && c_vec[i] > 5) {
+                    output << fixed << setprecision(2) << b_vec[i] << endl;
+                }
+            }
+            output.close();
+            cout << "Zapisano wartości b do pliku bok_b.txt\n";
+        }
+    }
+    else if (wybor == 0) {
+        cout << "Zakończono program.\n";
+    }
+    else {
+        cout << "Niepoprawny wybór.\n";
+    }
 
     return 0;
 }
